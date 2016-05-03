8925704f730389955c4c62fed5a28f44564fbfe1
guimiran <guimiran@LES001013.corp.capgemini.com>
2016-05-03 09:00:50 +0200

    new source
    Change-Id: Ifbd8785c068313e01e0a58c30ea1f220d1175d2e
    Signed-off-by: guimiran <guimiran@LES001013.corp.capgemini.com>

---
+++ cucumberConcept/src/features/StepDefinitions.java
@@ -0,0 +1,39 @@
+package features;
+
+import junit.framework.Assert;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinitions {
+	
+	WebDriver driver =null;
+	
+	
+	@Given("^this is my first step$")
+	public void posicionNavegador() throws Throwable {
+		//System.out.println("executed: Navegacion en el portal a testear ");
+		driver = new ChromeDriver();
+		driver.navigate().to("http://www.fib.upc.edu/es.html");
+	    
+	}
+
+	@When("^this is my second step$")
+	public void accionClickenBotton() throws Throwable {
+	    //System.out.println("executed: realizar una accion: click en un boton ");
+		driver.findElement(By.id("racoCarnet")).click();
+	}
+	
+	@Then("^this is my final step$")
+	public void comprobacionBotonPortal() throws Throwable {
+	    //System.out.println("check: comprobar que el dato es el esperado");
+		Assert.assertTrue("error no estas en la pagina que se esperaba", driver.getTitle().equals("Login - Facultat d'informàtica de Barcelona"));
+	    
+	}
+
+}
